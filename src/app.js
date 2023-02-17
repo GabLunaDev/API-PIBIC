728a3956e7005a54880a4b388e64f4666bd72301
GabLunaDev <gabriellunacv@gmail.com>
2023-02-17 19:34:52 -0300

    feat: route to evaluate assigned articles

--- src/app.js
+++ src/app.js
@@ -13,7 +13,20 @@ const { logMiddleware } = require("./middleware/LogMiddleware");
 try {
   app.use(logMiddleware);
   app.use(express.json());
-  app.use(cors());
+
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  }
+  
+  app.use(cors(corsOptions));
+
   app.use("/api", routes);
 
   app.use(express.urlencoded({ extended: true }));
